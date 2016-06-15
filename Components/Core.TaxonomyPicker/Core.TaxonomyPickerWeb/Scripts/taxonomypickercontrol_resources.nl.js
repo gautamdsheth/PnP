2dabca7ebdaba86ecce8d4c15386047b8316d489
Gautam Sheth <gautam.sheth@zevenseas.com>
2016-06-15 12:27:52 +0530

    Create taxonomypickercontrol_resources.nl.js
    Added dutch resources file

---
+++ Components/Core.TaxonomyPicker/Core.TaxonomyPickerWeb/Scripts/taxonomypickercontrol_resources.nl.js
@@ -0,0 +1,9 @@
+function TaxonomyPickerConsts() { };
+TaxonomyPickerConsts.SUGGESTIONS_HEADER = 'Suggesties';
+TaxonomyPickerConsts.DIALOG_HEADER = 'Selecteer : ';
+TaxonomyPickerConsts.BUTTON_TEXT = 'Selecteer';
+TaxonomyPickerConsts.AMBIGUOUS_ENTRY = 'Dubbelzinnige binnenkomst';
+TaxonomyPickerConsts.DIALOG_ADD_TITLE = 'Nieuwe items worden onder huidige item toegevoegd';
+TaxonomyPickerConsts.DIALOG_ADD_LINK = 'Nieuw item';
+TaxonomyPickerConsts.TERMSET_LOAD_FAILED = 'Fout opgetreden.  Aub vernieuw de pagina.';
+TaxonomyPickerConsts.WORKING_ON_IT = '&nbsp;Bezig verwerken...';
